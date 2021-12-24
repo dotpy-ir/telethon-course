fd8484b04667f6a1c15e0633a783d85a07a06dbb
Mohammadreza Jafari <fristhon97@gmail.com>
2021-12-24 21:48:29 +0330

    08 CallbackQuery event

---
+++ callback_query.py
@@ -0,0 +1,31 @@
+import asyncio
+from telethon import TelegramClient, events, Button
+from config import API_ID, API_HASH, TOKEN
+
+
+async def start_handler(event):
+    buttons = [
+        [Button.inline("Song", "song.mp3")],
+        [Button.inline("Picture", "dog.jpg")],
+    ]
+    await event.client.send_message(event.chat_id, "start", buttons=buttons)
+
+
+async def inline_handler(event):
+    data = event.data.decode()
+    print(data)
+    await event.client.send_message(event.chat_id, data, file=data)
+
+
+async def main():
+    client = TelegramClient("realBot", API_ID, API_HASH)
+    await client.start(bot_token=TOKEN)
+
+    client.add_event_handler(
+        inline_handler, events.CallbackQuery(pattern="song.+"))
+    client.add_event_handler(
+        start_handler, events.NewMessage(pattern="/start"))
+
+    await client.run_until_disconnected()
+
+asyncio.run(main())
